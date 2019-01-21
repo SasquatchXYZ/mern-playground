73e2cd02310f3b23df947bbd3b34779b8b22fd06
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-21 02:16:37 -0500

    working towards user authentication and logins for the Bookshelf

---
+++ client/src/pages/Bookshelf.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Bookshelf extends Component {
+  render() {
+    return (
+      <div class="container">
+        <h1>This is the BookShelf Page:</h1>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf;
