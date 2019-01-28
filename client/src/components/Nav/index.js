1ad1070362bbe84af8a5d45db8029f5c5701e813
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-28 18:44:55 -0500

    not sure where I am going with this right now...

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,37 +1,47 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Nav extends Component {
-  render() {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-    return (
-      <div>
-        <nav>
-          <div className="nav-wrapper">
-            <a href="#" className="brand-logo center">Logo</a>
-            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-            <ul className="left hide-on-med-and-down">
-              <li className="active"><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">Javascript</a></li>
-              <li><a href="mobile.html">Mobile</a></li>
-            </ul>
-            <ul className="right hide-on-med-and-down">
-              <li><a className="waves-effect waves-light btn">Button</a></li>
-            </ul>
-          </div>
-        </nav>
-
-        <ul className="sidenav" id="mobile-demo">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">Javascript</a></li>
-          <li><a href="mobile.html">Mobile</a></li>
-        </ul>
-
-      </div>
-    )
-  }
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Nav;
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const logger = require('morgan');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
@@ -8,6 +9,7 @@ mongoose.set('useCreateIndex', true);
 // Define Middleware Here -------------------------------------
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(logger('dev'));
 
 // Serve Up Static Assets (usually on Heroku) -----------------
 if (process.env.NODE_ENV === 'production') {
@@ -19,7 +21,7 @@ app.use(routes);
 
 // Establishing Connection to MongoDB Database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mernplayground';
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`API Server now on port: ${PORT}`)
